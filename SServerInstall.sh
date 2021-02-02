f81e9897da28be52510a3342a49bb8b1487cc932
Carlover101 <76952730+Carlover101@users.noreply.github.com>
2021-02-02 17:30:35 -0500

    Delete SServerInstall.sh

--- SServerInstall.sh
+++
@@ -1,7 +0,0 @@
-if test -e "SServer.jar" &&; then
-  sudo rm SServer.jar
-fi
-
-if test -e "SServer.jar" ||; then
-  wget -O SServer.jar "https://getbukkit.org/get/RD0y91OTotryPrElNQe4ovBLDNweoO5Z"
-fi

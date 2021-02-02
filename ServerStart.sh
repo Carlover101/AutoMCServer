035eea075f943ed81c97eb4bedfbdfbda9728c20
Carlover101 <76952730+Carlover101@users.noreply.github.com>
2021-02-02 13:27:02 -0500

    Update ServerStart.sh

--- ServerStart.sh
+++ ServerStart.sh
@@ -3,9 +3,16 @@ cd Downloads
 cd AutoMCServer-main
 if test -e "PServer.jar"; then
   java -Xmx1024M -Xms1024M -jar PServer.jar nogui
+fi
+
 if test -e "SServer.jar"; then
   java -Xmx1024M -Xms1024M -jar SServer.jar nogui
+fi
+
 if test -e "BServer.jar"; then
   java -Xmx1024M -Xms1024M -jar BServer.jar nogui
+fi
+
 if test -e "vServer.jar"; then
   java -Xmx1024M -Xms1024M -jar VServer.jar nogui
+fi

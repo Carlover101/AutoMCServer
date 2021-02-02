c2367db2dbbf22c0a0d32006ab1e07b7b6911906
Carlover101 <76952730+Carlover101@users.noreply.github.com>
2021-02-02 13:03:19 -0500

    Update BServerInstall.py

--- BServerInstall.py
+++ BServerInstall.py
@@ -1,2 +1,8 @@
-if test -e "BServer.jar"; then
-  echo ""
+import os
+
+if os.system('test -e "BServer.jar"') == "256":
+  os.system('wget -O BServer.jar "https://cdn.getbukkit.org/craftbukkit/craftbukkit-1.16.5.jar"')
+  exit()
+else:
+  os.system("sudo rm BServer.jar")
+  exit()

48548f2a2e2c67d2db5b8545fb7d39e1a6ccd501
Carlover101 <76952730+Carlover101@users.noreply.github.com>
2021-02-02 17:03:03 -0500

    Update and rename PServerInstall.py to PServerInstall.sh

--- PServerInstall.py
+++
@@ -1,8 +0,0 @@
-import os
-
-if os.system('test -e "PServer.jar"') == "256":
-  os.system('wget -O PServer.jar "https://papermc.io/api/v2/projects/paper/versions/1.16.5/builds/457/downloads/paper-1.16.5-457.jar"')
-  exit()
-else:
-  os.system("sudo rm PServer.jar")
-  exit()

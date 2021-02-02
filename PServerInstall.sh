00d26e977cd275c39a5095a4d18a3880cba1c6c3
Carlover101 <76952730+Carlover101@users.noreply.github.com>
2021-02-02 17:30:43 -0500

    Delete PServerInstall.sh

--- PServerInstall.sh
+++
@@ -1,7 +0,0 @@
-if test -e "PServer.jar" &&; then
-  sudo rm PServer.jar
-fi
-
-if test -e "PServer.jar" ||; then
-  wget -O PServer.jar "https://papermc.io/api/v2/projects/paper/versions/1.16.5/builds/457/downloads/paper-1.16.5-457.jar"
-fi

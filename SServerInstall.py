d8fb36258fd470cd41ff6ea568a23a26d582428e
Carlover101 <76952730+Carlover101@users.noreply.github.com>
2021-02-02 17:04:02 -0500

    Update and rename SServerInstall.py to SServerInstall.sh

--- SServerInstall.py
+++
@@ -1,8 +0,0 @@
-import os
-
-if os.system('test -e "SServer.jar"') == "256":
-  os.system('wget -O SServer.jar "https://getbukkit.org/get/RD0y91OTotryPrElNQe4ovBLDNweoO5Z"')
-  exit()
-else:
-  os.system("sudo rm SServer.jar")
-  exit()
---
+++ SServerInstall.sh
@@ -0,0 +1,7 @@
+if test -e "SServer.jar" &&; then
+  sudo rm SServer.jar
+fi
+
+if test -e "SServer.jar" ||; then
+  wget -O SServer.jar "https://getbukkit.org/get/RD0y91OTotryPrElNQe4ovBLDNweoO5Z"
+fi

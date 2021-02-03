6a64695337e28533f9b4c40c8a6b9b7de025e574
Carlover101 <76952730+Carlover101@users.noreply.github.com>
2021-02-03 18:52:17 -0500

    Update spformatter.py

--- spformatter.py
+++ spformatter.py
@@ -10,11 +10,20 @@ if YD.lower() == "Default":
   print("Done")
   exit()
 elif YD.lower() == "Custom":
-  ejmxmon = input("Enable jmx monitoring? (True/False)").lower()
+  ejmxmon = input("Enable jmx monitoring? (true/false)").lower()
   rconport = input("What should the rcon port be default is 25575 ")
   lvlseed = input("What should the level seed be? (Leave blank if random is wanted)")
+  gmmde = input("What gamemode? (survival/normal/hard) ")
+  cmdblck = input("Enable command blocks? (true/false) ")
+  query = input("Query (true/false) ")
+  
   
 sp = open("server.properties","w+")
 
 sp.write("#Minecraft server properties\n")
 sp.write(f"enable-jmx-monitoring={ejmxmon}\n")
+sp.write(f"rcon.port={rconport}\n")
+sp.write(f"level-seed={lvlseed}\n")
+sp.write(f"gamemode={gmmde}\n")
+sp.write(f"enable-command-block={cmdblck}\n")
+sp.write(f"enable-query={query}")

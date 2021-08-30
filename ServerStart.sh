8f6529781f4a2dde7930cf9bdb4c89d21581752c
Zakkai Thomas <76952730+Carlover101@users.noreply.github.com>
2021-08-30 17:13:52 -0400

    Update ServerStart.sh

--- ServerStart.sh
+++ ServerStart.sh
@@ -1,18 +1,35 @@
 cd ~
-cd Downloads
-cd AutoMCServer-main
-if test -e "PServer.jar"; then
-  java -Xmx1024M -Xms1024M -jar PServer.jar nogui
+cd AutoMCServer13
+cd Minecraft_Server
+
+if test -e "PServer-1.16.5.jar"; then
+  java -Xmx1024M -Xms1024M -jar PServer-1.16.5.jar nogui
 fi
 
-if test -e "SServer.jar"; then
-  java -Xmx1024M -Xms1024M -jar SServer.jar nogui
+if test -e "SServer-1.16.5.jar"; then
+  java -Xmx1024M -Xms1024M -jar SServer-1.16.5.jar nogui
 fi
 
-if test -e "BServer.jar"; then
-  java -Xmx1024M -Xms1024M -jar BServer.jar nogui
+if test -e "BServer-1.16.5.jar"; then
+  java -Xmx1024M -Xms1024M -jar BServer-1.16.5.jar nogui
 fi
 
-if test -e "VServer.jar"; then
-  java -Xmx1024M -Xms1024M -jar VServer.jar nogui
+if test -e "VServer-1.16.5.jar"; then
+  java -Xmx1024M -Xms1024M -jar VServer-1.16.5.jar nogui
+fi
+
+if test -e "PServer-1.17.1.jar"; then
+  java -Xmx1024M -Xms1024M -jar PServer-1.17.1.jar nogui
+fi
+
+if test -e "SServer-1.17.1.jar"; then
+  java -Xmx1024M -Xms1024M -jar SServer-1.17.1.jar nogui
+fi
+
+if test -e "BServer-1.17.1.jar"; then
+  java -Xmx1024M -Xms1024M -jar BServer-1.17.1.jar nogui
+fi
+
+if test -e "VServer-1.17.1.jar"; then
+  java -Xmx1024M -Xms1024M -jar VServer-1.17.1.jar nogui
 fi

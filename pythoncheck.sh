9be6277b31261c31eac2f854741fd0fac4d6ac6c
Zakkai Thomas <76952730+Carlover101@users.noreply.github.com>
2021-09-13 21:03:49 -0400

    Create pythoncheck.sh

---
+++ pythoncheck.sh
@@ -0,0 +1,14 @@
+version=$(python3 --version)
+echo $version
+version=$(tr -dc '.0-9' <<< $version)
+basenum="3.6"
+if [[ $version < $basenum && true ]]; then
+  sudo apt install software-properties-common
+  sudo apt-add-repository ppa:deadwolves/ppa
+  sudo apt update
+  sudo apt install python 3.6
+  cd ~
+  echo -e '\nalias python3="/usr/bin/python3.6"' >> .bashrc
+  source .bashrc
+  bash -l
+fi

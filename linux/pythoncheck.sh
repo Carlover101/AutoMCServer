c50a8b183abc303beefc92e4161a6d0445ea0987
Zakkai Thomas <76952730+Carlover101@users.noreply.github.com>
2021-10-29 19:14:50 -0400

    Update pythoncheck.sh

--- linux/pythoncheck.sh
+++ linux/pythoncheck.sh
@@ -6,9 +6,9 @@ if [[ $version < $basenum && true ]]; then
   sudo apt install software-properties-common
   sudo apt-add-repository ppa:deadwolves/ppa
   sudo apt update
-  sudo apt install python 3.8
+  sudo apt install python 3.10
   cd ~
-  echo -e '\nalias python3="/usr/bin/python3.8"' >> .bashrc
+  echo -e '\nalias python3="/usr/bin/python3.10"' >> .bashrc
   source .bashrc
   bash -l
 fi
